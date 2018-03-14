731ab56d7149fda995f400d3978c4158490b6fdb
Misti Gray <misti.mechelle@gmail.com>
2018-03-13 23:56:07 -0500

    update

---
+++ gifLogic.js
@@ -0,0 +1,65 @@
+//Array of musicians
+var topics = ["Beyonce", "Kanye","Rihanna", "Drake", "SZA", "Jay-Z", "Cardi B", "Taylor Swift"];
+
+function displayGifs(){
+    var gif = $(this).attr("data-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=VQ3pMYT3vAQaJuZ4LQb8zOkqWl6DzVlL&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function(response) {
+      
+        var results = response.data;
+
+        for (var i=0; i < results.length; i++) {
+        
+    
+      var gifDiv = $("<div class ='gifs'>");
+
+
+      var displayRating = $("<p>").text("Rating: " +results[i].rating);
+
+    
+    gifDiv.append(displayRating);
+    var gifImage = $("<img>"); 
+
+    gifImage.attr("src", results[i].images.fixed_height_still.url); 
+
+    
+
+    gifDiv.append(gifImage); 
+    //   gifImage.attr("src", results[i].images.fixed_height_still.url);
+
+    $("#gifs-view").prepend(gifDiv);
+
+    //animate: fixed_height_downsampled
+        }  
+    });
+}
+function renderButtons() {
+    $("buttons-view").empty();
+
+    for (var i=0; i < topics.length; i++) {
+        var add = $("<button>");
+        add.addClass("gif-btn");
+        add.addClass("data-name", topics[i]);
+        add.text(topics[i]);
+        $("#buttons-view").append(add);
+    }
+
+}
+
+$("add-gif").on("click", function(event){
+    event.preventDefault();
+    var gif = $("#gif-input").val().trim();
+    topics.push(gif);
+    renderButtons();
+});
+
+$(document).on("click", ".gif-btn", displayGifs);
+
+renderButtons();
+
